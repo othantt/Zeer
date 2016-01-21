6a3febe0761aa7c697546f281f92f9128c3a016d
Sploux39 <sploux39@yopmail.com>
2016-01-21 13:58:33 +0100

    Cookies clear for c_id request

--- app/src/main/java/www/zeer/DeezerAPI/DeezerTokenRequest.java
+++ app/src/main/java/www/zeer/DeezerAPI/DeezerTokenRequest.java
@@ -2,16 +2,20 @@ package www.zeer.DeezerAPI;
 
 import android.util.Log;
 
+import com.android.volley.Cache;
 import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.error.VolleyError;
 import com.android.volley.request.StringRequest;
+import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 
 import java.util.Map;
+
+import www.zeer.InitApplication;
 
 /**
  * Created by root on 20/01/16.
@@ -21,7 +25,10 @@ public class DeezerTokenRequest extends StringRequest{
     String cookie_session;
 
     public DeezerTokenRequest(int method, String url, Response.Listener listener, Response.ErrorListener Errorlistener){
+
         super(method, url, listener, Errorlistener);
+        InitApplication.getInstance().getRequestQueue().getCache().clear();
+
     }
 
 
@@ -31,8 +38,10 @@ public class DeezerTokenRequest extends StringRequest{
         Map<String, String> responseHeaders = response.headers;
         cookie_session = responseHeaders.get("Set-Cookie");
         DeezerAPI.setCookies(cookie_session);
-        Log.e("COOKI", cookie_session +"    cook" );
+        Log.e("Cookies", cookie_session);
         return r;
     }
+
+
 
 }
